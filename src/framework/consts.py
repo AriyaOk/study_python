efa6944896703c374521cdcd55cffddb4f004771
AriyaOk <ariyaOk@gmail.com>
2020-10-22 13:51:32 +0300

    name_fail i constant

--- src/framework/consts.py
+++ src/framework/consts.py
@@ -11,3 +11,6 @@ Visit http://{host}:{port}
 """
 
 dir_static = (Path(__file__).parent.parent / "static").resolve()
+file_scc = "styles.css"
+file_logo = "ariyaOk.gif"  # "logo.png"
+file_index = "index.html"
--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,39 +1,78 @@
+import mimetypes
+
 from framework.consts import dir_static
+from framework.consts import file_index
+from framework.consts import file_logo
+from framework.consts import file_scc
 
 
 def application(environ, start_response):
     url = environ["PATH_INFO"]
     if url == "/xxx/":
         status = "200 OK"
-        headers = {
-            "Content-type": "text/css",  # mimetypes.guess_type
-        }
-        payload = read_from_styles_css()
+        # headers = {
+        #    "Content-type": "text/css",
+        # }
+        # payload = read_from_styles_css()
+
+        # type_file_ = mimetypes.guess_type(file_scc)[0]
+        headers = set_headers(file_scc)  # "text/css"
+        payload = read_from_file(file_scc, "r", True)  # read_from_styles_css()
+
         start_response(status, list(headers.items()))
 
         yield payload
         # return None #можно без None - функции, если не указано явно, то возвращает None
     elif url == "/logo.png/":
         status = "200 OK"
-        headers = {
-            "Content-type": "image/png",
-        }
-        payload = read_from_styles_png()
+        # headers = {
+        #    "Content-type": "image/png",
+        # }
+        # payload = read_from_styles_png()
+
+        # type_file_ = mimetypes.guess_type(file_logo)[0]
+        headers = set_headers(file_logo)  # "image/png"
+        payload = read_from_file(file_logo, "rb", False)  # read_from_styles_png()
+
         start_response(status, list(headers.items()))
 
         yield payload
     else:
         status = "200 OK"
-        headers = {
-            "Content-type": "text/html",
-        }
-        payload = read_from_index_html()
+        # headers = {
+        #    "Content-type": "text/html",
+        # }
+        # payload = read_from_index_html()
+
+        # type_file_ = mimetypes.guess_type(file_index)[0]
+        headers = set_headers(file_index)  # "text/html"
+        payload = read_from_file(file_index, "r", True)  # read_from_index_html()
 
         start_response(status, list(headers.items()))
 
         yield payload
 
 
+def read_from_file(name_file, mode, encode_use):
+    path = dir_static / name_file
+    with path.open(mode) as fp:
+        payload = fp.read()
+
+    if encode_use:
+        payload = payload.encode()
+
+    return payload
+
+
+def set_headers(file_name):
+    type_file_ = mimetypes.guess_type(file_name)[0]
+    headers = {
+        "Content-type": type_file_,
+    }
+    return headers
+
+
+#############################################################
 def read_from_index_html():
     path = dir_static / "index.html"
     with path.open("r") as fp:
