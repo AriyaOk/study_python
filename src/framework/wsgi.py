a13c9900f6bcce19a41b379002c0c3acf1679157
AriyaOk <ariyaOk@gmail.com>
2020-10-23 20:36:49 +0300

    supp

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -2,13 +2,14 @@ import mimetypes
 
 from framework.consts import dir_static
 
+
 def application(environ, start_response):
     url = environ["PATH_INFO"]
     file_names = {
-        "/xxx/":"styles.css",
+        "/xxx/": "styles.css",
         "/logo.png/": "ariyaOk.gif",
     }
-    file_name = file_names.get(url,"index.html")
+    file_name = file_names.get(url, "index.html")
     status = "200 OK"
     type_file_ = mimetypes.guess_type(file_name)[0]
     headers = {
@@ -20,9 +21,7 @@ def application(environ, start_response):
     yield payload
 
 
-
-
-def read_static(name_file:str) -> bytes:
+def read_static(name_file: str) -> bytes:
     path = dir_static / name_file
     with path.open("rb") as fp:
         payload = fp.read()
@@ -30,8 +29,4 @@ def read_static(name_file:str) -> bytes:
     return payload
 
 
-
-
-
 #############################################################
-
