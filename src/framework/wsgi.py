d6ad9ee9a9feaaa2357dde18fced5d57cca04ee1
AriyaOk <ariyaOk@gmail.com>
2020-10-26 12:13:55 +0300

    error_

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,4 +1,5 @@
 import mimetypes
+import random
 
 from framework.consts import dir_static
 
@@ -8,14 +9,23 @@ def application(environ, start_response):
     file_names = {
         "/xxx/": "styles.css",
         "/logo.png/": "ariyaOk.gif",
+        "/": "index.html",
     }
-    file_name = file_names.get(url, "index.html")
-    status = "200 OK"
-    type_file_ = mimetypes.guess_type(file_name)[0]
-    headers = {
-        "Content-type": type_file_,
-    }
-    payload = read_static(file_name)
+    file_name = file_names.get(url,)
+
+    if file_name is not None:
+        status = "200 OK"
+        type_file_ = mimetypes.guess_type(file_name)[0]
+        headers = {
+            "Content-type": type_file_,
+        }
+        payload = read_static(file_name)
+    else:
+        status = "404 not found"
+        headers = {
+            "Content-type": "text/plain",
+        }
+        payload = generate_404(environ)
 
     start_response(status, list(headers.items()))
     yield payload
@@ -29,4 +39,10 @@ def read_static(name_file: str) -> bytes:
     return payload
 
 
-#############################################################
+def generate_404(environ) ->bytes:
+    url = environ["PATH_INFO"]
+    pin = random.randint(1, 1000)
+
+    msg = f"Error! Your path: {url}. Pin: {pin}"
+
+    return msg.encode()
