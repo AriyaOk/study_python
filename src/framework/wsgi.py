6f4164b1bd8f2339c4cdee1413a95ec7355b2f46
AriyaOk <ariyaOk@gmail.com>
2020-11-06 21:14:40 +0300

    222

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,11 +1,11 @@
 from urllib.parse import parse_qs
 
 from framework.types import RequestT
+from handlers import handle_hello
 from handlers import handle_index
 from handlers import handle_logo
 from handlers import handle_style
 from handlers import handler_404
-from handlers import handle_hello
 from handlers import handler_500
 from handlers.test_error import make_error
 
--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,7 +1,6 @@
 from .handler_error404 import handler_404
+from .handler_hello import handle_hello
 from .handler_index import handle_index
 from .handler_logo import handle_logo
 from .handler_style import handle_style
-from .handler_hello import handle_hello
 from .hendler_error500 import handler_500
-
