56614479a43b4a12051a99f7fb4a6080df74ec32
AriyaOk <ariyaOk@gmail.com>
2020-11-06 21:12:49 +0300

    222

--- src/handlers/handler_hello.py
+++ src/handlers/handler_hello.py
@@ -4,10 +4,10 @@ from framework.utils import read_static
 
 
 def handle_hello(request: RequestT) -> ResponseT:
-    name = (request.query.get("name") or ["anon"])[0]
+    name = (request.query.get("name") or [None])[0]
     base_html = read_static("_base.html").decode()
-    index_html = f"<h1>Hello {name}</h1>"
-
+    index_html = read_static("hello_html.html").decode()#f"<h1>Hello {name}</h1>"
+    index_html = index_html.format(name_hader=name or "anon",name_value=name or "")
     result = base_html.format(body_=index_html)
     result = result.encode()
 
