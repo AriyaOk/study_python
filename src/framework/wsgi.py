8cad59ddaffff1a07763e7c8b87e78e035362340
AriyaOk <ariyaOk@gmail.com>
2020-11-06 12:07:56 +0300

    222

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,37 +1,48 @@
+from urllib.parse import parse_qs
+
 from framework.types import RequestT
 from handlers import handle_index
 from handlers import handle_logo
 from handlers import handle_style
 from handlers.handler_error import handler_404
+from handlers.handler_hello import handle_hello
+from handlers.server_error import handler_500
+from handlers.test_error import make_error
 
 handlers_ = {
     "/logo.png/": handle_logo,
     "/xxx/": handle_style,
     "/": handle_index,
+    "/e": make_error,
+    "/h": handle_hello,
 }
 
 
 def application(environ, start_response):
-    path = environ["PATH_INFO"]
+    try:
+        path = environ["PATH_INFO"]
 
-    handler = handlers_.get(path, handler_404)
+        handler = handlers_.get(path, handler_404)
 
-    # keys = list(environ.keys())
-    # request_key_http = [
-    #     key
-    #     for key in environ
-    #     if key.startswith("HTTP")
-    # ]
-    request_key_http = filter(lambda key: key.startswith("HTTP"), environ)
+        # keys = list(environ.keys())
+        # request_key_http = [
+        #     key
+        #     for key in environ
+        #     if key.startswith("HTTP")
+        # ]
+        request_key_http = filter(lambda key: key.startswith("HTTP"), environ)
 
-    request_headers = {key[5:]: environ[key] for key in request_key_http}
+        request_headers = {key[5:]: environ[key] for key in request_key_http}
 
-    request = RequestT(
-        method=environ["REQUEST_METHOD"],
-        path=path,
-        headers=request_headers,
-    )
-    response = handler(request)
+        request = RequestT(
+            method=environ["REQUEST_METHOD"],
+            path=path,
+            headers=request_headers,
+            query=parse_qs(environ.get("QUERY_STRING") or ""),
+        )
+        response = handler(request)
+    except:
+        response = handler_500()
 
     start_response(response.status, list(response.headers.items()))
     yield response.payload
---
+++ src/handlers/server_error.py
@@ -0,0 +1,43 @@
+import sys
+import traceback
+
+from framework.types import RequestT
+from framework.types import ResponseT
+from framework.utils import read_static
+
+
+def handler_500(_request: RequestT = None) -> ResponseT:
+    error_class, error, tb = sys.exc_info()
+    filenames = "".join(
+        f"""<p>File <a href="http://localhost:8000/s/{frame.f_code.co_filename}">{frame.f_code.co_filename}</a>, line {lineno}</p>"""
+        for frame, lineno in traceback.walk_tb(tb)
+    )
+
+    # result = traceback.format_exc()
+    document = f"""
+            <h1>WASTED</h1>
+            <hr>
+            <p>
+            {filenames}
+            </p>
+            <p>
+            {error_class.__name__}: {error}
+            </p>
+        """
+    base_html = read_static("_base.html").decode()
+    result = base_html.format(body_=document)
+    result = result.encode()
+
+    status = "500 Internal Server Error"
+
+    headers = {
+        "Content-type": "text/html",
+    }
+
+    response = ResponseT(
+        status=status,
+        headers=headers,
+        payload=result,
+    )
+
+    return response
