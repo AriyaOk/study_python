0d7c35b9c257c68463fa7683d299ea2c68fad895
AriyaOk <72444359+AriyaOk@users.noreply.github.com>
2020-11-27 18:07:18 +0300

    Lesson16 (#22)
    * hello
    
    * hello
    
    * hello

--- src/application/hello/views.py
+++ src/application/hello/views.py
@@ -8,12 +8,15 @@ def view_hello_index(request: HttpRequest) -> HttpResponse:
 
     name = request.session.get("name")
     address = request.session.get("address")
+    about = request.session.get("about")
 
     context = {
         "address_header": address or "XZ",
         "address_value": address or "",
         "name_header": name or "Anon",
         "name_value": name or "",
+        "about_header": about or "no information",
+        "about_value": about or "",
     }
     response = render(request, "hello/index.html", context=context)
     return response
@@ -22,9 +25,11 @@ def view_hello_index(request: HttpRequest) -> HttpResponse:
 def view_hello_greet(request: HttpRequest) -> HttpResponse:
     name = request.POST.get("name")
     address = request.POST.get("address")
+    about = request.POST.get("about")
 
     request.session["name"] = name
     request.session["address"] = address
+    request.session["about"] = about
 
     return redirect("/h/")
 
